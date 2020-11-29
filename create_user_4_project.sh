07e4fe1ed5603f6cdaa61d8a167aa96d7d7e51b0
Nikita Pershukov <nikita.pershukov@gmail.com>
2020-11-30 00:01:06 +0300

    Touching rm_file.txt

--- create_user_4_project.sh
+++ create_user_4_project.sh
@@ -6,6 +6,7 @@ if [ $# -eq 1 ]; then
     sudo passwd "$1"
     sudo mkdir -p "/data/$1/app/prod"
     sudo mkdir -p "/data/$1/app/backup"
+    sudo touch "/data/$1/app/prod/rm_file.txt"
     sudo ln -s "/data/$1" "/home/$1/project"
     sudo ln -s "/data/hseling-helm" "/data/$1/helm"
     sudo chown -R "$1":"hse_linghub_k8s" "/data/$1"
