89fba01204b5d539a9702a3dbb850dabb89fb94d
Nikita Pershukov <nikita.pershukov@gmail.com>
2020-11-30 00:07:23 +0300

    Fixing access for deploy

--- install.sh
+++ install.sh
@@ -10,19 +10,19 @@ if [ $# -ge 3 ]; then
 fi
 
 PROJECT_NAME="$1"
-FULL_PROJECT_NAME="hseling-$PROJECT_NAME"
-PROJECT_NAMESPACE="$FULL_PROJECT_NAME"
+FULL_PROJECT_NAME="hseling-${PROJECT_NAME}"
+PROJECT_NAMESPACE="${FULL_PROJECT_NAME}"
 
-PROJECT_EXISTS="$(helm list -n $PROJECT_NAMESPACE | grep $FULL_PROJECT_NAME | cut -f 1)"
+PROJECT_EXISTS="$(helm list -n ${PROJECT_NAMESPACE} | grep ${FULL_PROJECT_NAME} | cut -f 1)"
 
 DOMAIN="${DOMAIN:-hseling}"
 
-IMAGE_API="hseling/hseling-api-$PROJECT_NAME"
-IMAGE_WEB="hseling/hseling-web-$PROJECT_NAME"
+IMAGE_API="hseling/hseling-api-${PROJECT_NAME}"
+IMAGE_WEB="hseling/hseling-web-${PROJECT_NAME}"
 
-VALUES="${VALUES},nameOverride=$FULL_PROJECT_NAME"
-VALUES="${VALUES},images.API.repository=$IMAGE_API"
-VALUES="${VALUES},images.Web.repository=$IMAGE_WEB"
+VALUES="${VALUES},nameOverride=${FULL_PROJECT_NAME}"
+VALUES="${VALUES},images.API.repository=${IMAGE_API}"
+VALUES="${VALUES},images.Web.repository=${IMAGE_WEB}"
 VALUES="${VALUES},ingress.enabled=true,ingress.hosts.0.host=${PROJECT_NAME}.${DOMAIN},ingress.hosts.0.paths=/"
 
 if [ "x${HSELING_K8S}" == "x" ]; then
@@ -34,10 +34,10 @@ VALUES="${VALUES},localPath=${2:-/data/${FULL_PROJECT_NAME}}"
 
 BASE_DIR=$(dirname $(readlink -e $0))
 
-echo $DIR
+sudo chown -R "$1":"hse_linghub_k8s" "/data/${FULL_PROJECT_NAME}"
 
-if [ -z $PROJECT_EXISTS ]; then
-  helm install $FULL_PROJECT_NAME $BASE_DIR --create-namespace --namespace $PROJECT_NAMESPACE --set $VALUES
+if [ -z ${PROJECT_EXISTS} ]; then
+  helm install ${FULL_PROJECT_NAME} ${BASE_DIR} --create-namespace --namespace ${PROJECT_NAMESPACE} --set ${VALUES}
 else
-  helm upgrade $FULL_PROJECT_NAME $BASE_DIR --namespace $PROJECT_NAMESPACE --set $VALUES
+  helm upgrade ${FULL_PROJECT_NAME} ${BASE_DIR} --namespace ${PROJECT_NAMESPACE} --set ${VALUES}
 fi
